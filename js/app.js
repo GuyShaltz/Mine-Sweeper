6eb193dc921e8f1d7fdc86286d24e3c534b63369
GuyShaltz <118910752+GuyShaltz@users.noreply.github.com>
2022-11-24 21:01:07 +0200

    Add files via upload
    thursday 21:00

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ var gBoard = []
 
 var gLevel = {
     SIZE: 4,
-    MINES: 2
+    MINES: 2,
+    life: 1
 }
 
 var gGame = {
@@ -17,11 +18,12 @@ var gGame = {
     shownCount: 0,
     markedCount: 0,
     secsPassed: 0,
-    minesCount: 0,
+    shown: 0,
     markedCount: 0,
-    life: 2
+    mineCells: []
 
 }
+var gShownCellsCount
 
 // ----FUNCTIONS:----
 function initGame() {
@@ -30,8 +32,10 @@ function initGame() {
         shownCount: 0,
         markedCount: 0,
         secsPassed: 0,
-        minesCount: gLevel.MINES,
-        markedCount: 0
+        shown: gLevel.MINES,
+        markedCount: 0,
+        life: gLevel.life,
+        mineCells: []
     }
     gBoard = buildBoard()
     console.log(gBoard);
@@ -44,6 +48,7 @@ function buildBoard() {
 
         for (var j = 0; j < gLevel.SIZE; j++) {
             var cell = gBoard[i][j]
+            if (cell.isMine) gGame.mineCells.push({ i: i, j: j })
             cell.minesAroundCount = setMinesNegsCount(i, j, gBoard)
         }
     }
@@ -135,8 +140,14 @@ function cellClicked(elCell, i, j) {
     if (!cell.isShown) { // if not false= true
         cell.isShown = true
         if (!cell.isMine) { //// if not false= true
+            gGame.shown++
             if (cell.minesAroundCount !== 0) cellValue = cell.minesAroundCount
-        } else cellValue = MINE
+        } else {
+            cellValue = MINE
+            // gGame.minesCount++
+            gGame.life--
+            checkGameOver()
+        }
         elCell.classList.add('shown')
         renderCell(elCell, cellValue)
     } return
@@ -175,6 +186,7 @@ function setTimer() {
 
 function cellMarked(elCell, i, j) {
     var cell = gBoard[i][j]
+    if (cell.isShown) return
     if (cell.isMarked) {
         cell.isMarked = false
         gGame.markedCount--
@@ -193,31 +205,78 @@ function setLevel(difficulty) {
         case ('Beginner'):
             gLevel.SIZE = 4
             gLevel.MINES = 2
-            gGame.life = 2
+            gLevel.life = 2
             break;
         case ('Medium'):
             gLevel.SIZE = 8
             gLevel.MINES = 14
-            gGame.life = 3
+            gLevel.life = 3
             break;
         case ('Advanced'):
             gLevel.SIZE = 12
             gLevel.MINES = 32
-            gGame.life = 3
+            gLevel.life = 3
             break;
         default:
             gLevel.SIZE = 2
             gLevel.MINES = 4
-            gGame.life = 2
+            gLevel.life = 1
     }
     initGame()
 }
 
 function checkGameOver() {
-    if (gGame.markedCount === gLevel.MINES) {
-        console.log('Game Over');
-    }
+    var cellCount = gLevel.SIZE ** 2
+    if (gGame.markedCount === gLevel.MINES || cellCount - gLevel.shown === gLevel.MINES) {
+        console.log('Winner');
 
+    } else if (gGame.life === 0) {
+        endGame()
+        console.log('GameOver');
+    }
+    else return
+
+}
+
+function endGame() {
+    gGame.isOn = false
+    clearInterval(gInterval)
+    for (var i = 0; i < gGame.mineCells; i++) {
+        var mineIdx = gGame.mineCells[i]
+        var cell = gBoard[mineIdx.i][mineIdx.j]
+    }
+}
+
+function revealMines() {
+    for (var i = 0; i < gGame.mineCells; i++) {
+        var pos = gGame.mineCells[i]
+        var cell = gBoard[pos.i][pos.j]
+
+        cell.isShown = true
+        var elCell = document.querySelector('id' + pos.i + pos.j)
+
+
+    }
+}
+
+
+
+function expandShown(board, currI, currJ) {
+    for (var i = currI - 1; i <= currI + 1; i++) {
+        if (i < 0 || i > board.length - 1) continue;
+        for (var j = currJ - 1; j <= currJ + 1; j++) {
+            if (j < 0 || j > board[0].length - 1) continue;
+            var cell = board[i][j];
+            if (!cell.isShown && !cell.isMarked) {
+                gShownCellsCount++;
+                cell.isShown = true;
+                if (board[i][j].minesAroundCount === 0) {
+                    expandShown(board, i, j);
+                }
+            }
+        }
+    }
+    return gShownCellsCount;
 }
 
 
